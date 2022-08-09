7f8695b42cd64de72ef711cd02cff7c21532be2a
kbiganashvili <106513663+kbiganashvili@users.noreply.github.com>
2022-08-09 11:53:06 +0400

    Fixed conflicts

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Product.css';
 
-<<<<<<< Updated upstream
-let localBasket = localStorage.getItem('Basket');
-=======
 const Product = (props) => {
   const { allProducts, setBasket, element, changeSelectedAmount } = props;
   const { id, src, name, price, stock, amount } = element;
-<<<<<<< Updated upstream
-=======
+
+  let localBasket = JSON.parse(localStorage.getItem('Basket')) || {};
 
   const setSelected = (id) => {
     allProducts.forEach((oldProduct) => {
@@ -16,55 +13,47 @@ const Product = (props) => {
         if (!oldProduct.amount) {
           oldProduct.amount = 1;
         }
-
         setBasket(id, oldProduct.amount);
       }
     });
   };
->>>>>>> Stashed changes
 
-  const setSelected = (id) => {
-    allProducts.forEach((oldProduct) => {
-      if (oldProduct.id === id) {
-        if (!oldProduct.amount) {
-          oldProduct.amount = 1;
+  return (
+    <div className="main-container">
+      <div
+        className={
+          localBasket.hasOwnProperty(id)
+            ? 'selected-product-box'
+            : 'unselected-product-box'
         }
-
-        setBasket(id, oldProduct.amount);
-      }
-    });
-  };
->>>>>>> Stashed changes
-
-export class Product extends Component {
-  render = () => {
-    const { element } = this.props;
-    const { src, name, price, stock, amount } = element;
-
-    return (
-      <div className="main-container">
-        <div
-          className={
-            localBasket.hasOwnProperty(1)
-              ? 'selected-product-box'
-              : 'unselected-product-box'
-          }
-        >
-          <img src={src} alt="product" className="productImg" />
-          <p className="productName">{name}</p>
-          <p className="productPrice">Price: ${price}</p>
-          <p className="productStock">Stock: {stock}</p>
-          <div className="productSelection">
-            <button type="submit" className="addProduct">
-              <i className="fa-solid fa-circle-minus"></i>
-            </button>
-            <p className="selectedProduct">{amount || 1}</p>
-            <button type="submit" className="removeProduct">
-              <i className="fa-solid fa-circle-plus"></i>
-            </button>
-          </div>
+      >
+        <img
+          src={src}
+          alt="product"
+          className="productImg"
+          onClick={() => setSelected(id)}
+        />
+        <p className="productName">{name}</p>
+        <p className="productPrice">Price: ${price}</p>
+        <p className="productStock">Stock: {stock}</p>
+        <div className="productSelection">
+          <button type="submit" className="addProduct">
+            <i
+              className="fa-solid fa-circle-minus"
+              onClick={() => changeSelectedAmount('decrement', id)}
+            ></i>
+          </button>
+          <p className="selectedProduct">{amount || 1}</p>
+          <button type="submit" className="removeProduct">
+            <i
+              className="fa-solid fa-circle-plus"
+              onClick={() => changeSelectedAmount('increment', id)}
+            ></i>
+          </button>
         </div>
       </div>
-    );
-  };
-}
+    </div>
+  );
+};
+
+export default Product;
